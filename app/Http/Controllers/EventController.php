af28c6dcb30aaeca95fd27ed05ff57164c1daa47
euliberato <ruanlib.2003@gmail.com>
2022-10-15 11:03:20 -0300

    Finalizando a pagina de criar eventos, criação de rota e controller (classe que irá receber os dados do evento)

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -20,6 +20,18 @@ class EventController extends Controller
         return view('events.create');
     }
 
+    public function store(Request $request) {
+        $event = new Event;
+        $event->title = $request->title;
+        $event->city = $request->city;
+        $event->private = $request->private;
+        $event->description = $request->description;
+
+        $event->save();
+
+        return redirect('/');
+    }
+
     public function events() {
         return view('events');
     }
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,7 +4,31 @@
 
 @section('content')
 
-<h1>Crie um evento</h1> 
-<a href="/">Retornar</a>
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie o seu evento</h1>
+    <form action="/events" method="POST">
+        @csrf
+        <div class="form-groups">
+            <label for="title">Evento:</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+        </div>
+        <div class="form-groups">
+            <label for="title">Cidade:</label>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
+        </div>
+        <div class="form-groups">
+            <label for="title">O evento é privado?</label>
+            <select name="private" id="private" class="form-control">
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select>
+        </div>
+        <div class="form-groups">
+            <label for="title">Descrição:</label>
+            <textarea name="description" id="description" class="form-control" placeholder="Uma breve descrição do seu evento..."></textarea>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Criar evento">
+    </form>
+</div>
 
 @endsection
