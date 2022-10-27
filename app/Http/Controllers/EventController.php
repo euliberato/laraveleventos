48d5cb72860d3889ff61f742597f9c81f8ec3b4b
euliberato <ruanlib.2003@gmail.com>
2022-10-27 10:03:41 -0300

    Criação da migrate de relação | Criação da rota de confirmaçao de presença | Criação do model users e evento com participante (relação) | Modificação da view com diretiva @csrf para formulario de confirmação

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -117,4 +117,16 @@ class EventController extends Controller
 
     }
 
+    public function joinEvent($id) {
+
+        $user = auth()->user();
+        $user->eventsAsParticipant()->attach($id);
+        
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Sua presença foi confirmada no evento: ' . $event->title);
+
+
+    }
+
 }
